03eb89f34bc9d8ddb22bf9efb6f4eaf14f8822b8
nathaliaassis <nathaliamassis@gmail.com>
2021-07-31 13:12:55 -0300

    reading csv file

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,11 +1,37 @@
-import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+import fs from 'fs';
+import csvParse from 'csv-parse';
+import { ICategoriesRepository } from "modules/cars/repositories/ICategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
 
 class ImportCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) { }
 
-  execute(file: any) {
-    console.log(file);
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      parseFile.on("data", async (line) => {
+        const [name, description] = line;
+
+        categories.push({ name, description });
+      }).on("end", () => {
+        resolve(categories);
+      }).on("err", (err) => reject(err));
+    });
+  }
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log(categories);
   }
 }
 
-export { ImportCategoryUseCase };
+export { ImportCategoryUseCase }; 
