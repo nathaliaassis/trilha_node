b0ba28e4ce3d5f0ab4ba292ab7393e9e3e175e1c
nathaliaassis <nathaliamassis@gmail.com>
2021-08-03 15:34:39 -0300

    password hash

--- src/modules/accounts/useCases/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { hash } from 'bcryptjs';
 import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
 import { IUsersRepository } from '../repositories/IUsersRepository';
 
@@ -10,9 +11,11 @@ class CreateUserUseCase {
   ) { }
 
   async execute({ name, password, email, driver_license }: ICreateUserDTO): Promise<void> {
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
-      password,
+      password: passwordHash,
       email,
       driver_license
     })
