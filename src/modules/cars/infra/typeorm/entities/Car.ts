5ec9b1c7354fa918f65182625c0874cc3284e03d
nathaliaassis <nathaliamassis@gmail.com>
2021-10-13 22:16:52 -0300

    estrutura de carros

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -18,7 +18,7 @@ class Car {
   daily_rate: number;
 
   @Column()
-  available = true;
+  available: boolean;
 
   @Column()
   license_plate: string;
@@ -43,6 +43,7 @@ class Car {
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
+      this.available = true;
     }
   }
 }
