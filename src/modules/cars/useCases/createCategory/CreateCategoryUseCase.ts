f10374dc45292ad44368bc0bc2b43565bb21f2b6
nathaliaassis <nathaliamassis@gmail.com>
2021-10-14 11:44:58 -0300

    comentário

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -9,7 +9,7 @@ interface IRequest {
 //regras de negócio da criação de uma categoria
 // as rotas não devem tratar condições, então criamos uma camada de serviço (Regras de negócio/ caso de uso) para 
 // tratar isso,
-// assim como, os services não devem retornar response e sim Error
+// assim como os services, não devem retornar response e sim Error
 @injectable()
 class CreateCategoryUseCase {
   // no meu constructor recebo o repositorio
